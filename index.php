f27eae62520c8419fd064b28006ca8e28646a31d
Andoni <andoni.losada@gmail.com>
2021-09-09 19:46:17 +0200

    Nuevo commit para comprobar funcionamiento VSC

--- index.php
+++ index.php
@@ -12,9 +12,7 @@
         print "Bienvenido al curso de PHP<br>";
         print "Holaalumnos<br>";
         print "Nos vemos en el próximo video";
-
-
-
+        print "Ea es una nueva modificación del index.html"
 
     ?>
     
